6e6566b030bc4163fb40aeca233691f17977de4f
Manish Gupta <jgupt29@ADTICORP.COM>
2026-04-01 09:31:22 +0530

    test comment

--- Controllers/TicketController.cs
+++ Controllers/TicketController.cs
@@ -33,7 +33,7 @@ public class TicketController : ControllerBase
         }
     }
 
-//31-03-2026 Test 5
+//31-03-2026 Test *8
     [HttpPost(ApiRoutes.Ticket.InsertTicketDetials)]
     public async Task<IActionResult> InsertTicketDetials([FromBody] InsertTicketDetialsRequest request)
     {
